68475249f2b1cb1f1bbdd7127ed2b3db64982492
p1ck23 <dagobbo17@gmail.com>
2024-10-30 14:48:23 +0400

    bug fix

--- js/main.js
+++ js/main.js
@@ -217,8 +217,6 @@ const accordionItemHeaders = document.querySelectorAll(
 
 accordionItemHeaders.forEach((accordionItemHeader) => {
   accordionItemHeader.addEventListener("click", (event) => {
-    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-
     const currentlyActiveAccordionItemHeader = document.querySelector(
       ".accordion-item-header.active"
     );
